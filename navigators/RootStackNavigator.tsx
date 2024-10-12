6ba64d242a20e7f149fbe87f3aeeaa7df9b08bb9
mrKornelius <magnus.korneliusson@gmail.com>
2024-10-12 11:32:17 +0200

    Profile header done

--- navigators/RootStackNavigator.tsx
+++ navigators/RootStackNavigator.tsx
@@ -1,16 +1,57 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, View } from "react-native";
+import { Button, IconButton, Text } from "react-native-paper";
 import HomeScreen from "../screens/HomeScreen";
+import ProfileScreen from "../screens/ProfileScreen";
 
 type rootStackParamList = {
-    Home: undefined;
-}
+  Home: undefined;
+  Profile: undefined;
+};
 
-const rootStack = createNativeStackNavigator<rootStackParamList>();
+const RootStack = createNativeStackNavigator<rootStackParamList>();
 
 export default function RootStackNavigator() {
-    return (
-        < rootStack.Navigator >
-            <rootStack.Screen name="Home" component={HomeScreen}></rootStack.Screen>
-        </rootStack.Navigator >
-    );
+  return (
+    <RootStack.Navigator>
+      <RootStack.Screen name="Home" component={HomeScreen} />
+      <RootStack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={({ navigation }) => ({
+          headerTitle: () => (
+            <View style={s.titleContainer}>
+              <Text style={s.title}>Profile</Text>
+              <IconButton
+                icon="account-edit-outline"
+                size={24}
+                // TODO: change route to edit-profile
+                onPress={() => navigation.navigate("Home")}
+              />
+            </View>
+          ),
+          headerRight: () => (
+            <Button
+              mode="contained"
+              // TODO: change route to logout? add confirmation?
+              onPress={() => navigation.navigate("Home")}
+            >
+              Logout
+            </Button>
+          ),
+        })}
+      />
+    </RootStack.Navigator>
+  );
 }
+
+const s = StyleSheet.create({
+  titleContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+  },
+});
