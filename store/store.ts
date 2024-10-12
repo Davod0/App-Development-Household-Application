ef0c3a0b015fa34936bfd115084093e42a806eaa
Davod <davodnikoyii@gmail.com>
2024-10-12 17:02:59 +0200

    Created userReducer and use it in store

--- store/store.ts
+++ store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { useReducer } from 'react'
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        user: useReducer,
+    },
 })
 
 export type RootState = ReturnType<typeof store.getState>
---
+++ store/userReducer.ts
@@ -0,0 +1,30 @@
+import type { PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+import { User } from '../data'
+
+
+const initialState: User = {
+    id: "",
+    firstName: "",
+    lastName: ""
+}
+
+export const UserSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        createUser: (state, action: PayloadAction<User>) => {
+            state.id = action.payload.id
+            state.firstName = action.payload.firstName
+            state.lastName = action.payload.lastName
+        },
+        clearUser: (state, action: PayloadAction<User>) => {
+            state.id = ""
+            state.firstName = ""
+            state.lastName = ""
+        },
+    },
+})
+
+export const { createUser, clearUser } = UserSlice.actions
+export default UserSlice.reducer
