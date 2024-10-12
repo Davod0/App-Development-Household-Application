90178b90e3aa0fc7e46dcf9ab42e4b183922a229
mrKornelius <magnus.korneliusson@gmail.com>
2024-10-12 16:36:18 +0200

    finished profile screen

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,10 +1,56 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { StyleSheet, View } from "react-native";
-import { Text } from "react-native-paper";
+import { Avatar, Button, Icon, Text } from "react-native-paper";
+import { avatarList, mockedMembers, mockedUsers } from "../data";
+import { RootStackParamList } from "../navigators/RootStackNavigator";
 
-export default function ProfileScreen() {
+type Props = NativeStackScreenProps<RootStackParamList, "Profile">;
+
+export default function ProfileScreen({ navigation }: Props) {
+  const userId = "user-2";
+  const user = mockedUsers.find((u) => u.id === userId);
+  const member = mockedMembers.find((m) => m.userId === userId);
+
+  if (!user || !member) {
+    throw new Error("bad userId: " + userId);
+  }
+
+  const avatar = avatarList.find((a) => a.id === member.avatarId)?.icon;
+
+  if (!avatar) {
+    throw new Error("bad avatarId:" + member.avatarId);
+  }
+
   return (
     <View style={s.container}>
-      <Text>ProfileScreen</Text>
+      <View style={s.memberInfo}>
+        <Avatar.Text size={192} label={avatar} />
+        <Text style={s.name}>{user.firstName + " " + user.lastName}</Text>
+        <Button mode="contained" onPress={() => {}}>
+          Byt Hushåll
+        </Button>
+        <Button mode="contained" onPress={() => {}}>
+          Hushålls info
+        </Button>
+      </View>
+      <View style={s.footer}>
+        <Button
+          icon={({ color }) => (
+            <Icon source="close-circle-outline" size={36} color={color} />
+          )}
+          mode="contained-tonal"
+          onPress={() => {
+            navigation.goBack();
+          }}
+          labelStyle={{
+            fontSize: 24,
+            lineHeight: 30,
+          }}
+          contentStyle={{ height: 65, gap: 10 }}
+        >
+          Stäng
+        </Button>
+      </View>
     </View>
   );
 }
@@ -12,7 +58,21 @@ export default function ProfileScreen() {
 const s = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "space-between",
     alignItems: "center",
+    marginVertical: 10,
+    marginHorizontal: 20,
+  },
+  memberInfo: {
+    flex: 1,
+    gap: 10,
+    alignItems: "center",
+  },
+  name: {
+    fontSize: 32,
+  },
+  footer: {
+    alignSelf: "flex-end",
+    width: "50%",
   },
 });
