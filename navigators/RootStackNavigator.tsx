9767ff87e7d67e6ebeeb23b51d05f038a73fe893
mrKornelius <magnus.korneliusson@gmail.com>
2024-10-12 16:46:08 +0200

    easier to change start screen

--- navigators/RootStackNavigator.tsx
+++ navigators/RootStackNavigator.tsx
@@ -13,7 +13,7 @@ const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootStackNavigator() {
   return (
-    <RootStack.Navigator>
+    <RootStack.Navigator initialRouteName="Home">
       <RootStack.Screen name="Home" component={HomeScreen} />
       <RootStack.Screen
         name="Profile"
