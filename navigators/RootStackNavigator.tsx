1fc32123603ad67b77c73e7148c0cf10d97b20c6
Alireza177 <alirezamahdavi015@gmail.com>
2024-10-13 13:30:18 +0200

    Create RegisterScreen

--- navigators/RootStackNavigator.tsx
+++ navigators/RootStackNavigator.tsx
@@ -1,8 +1,10 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
+import RegisterScreen from "../screens/RegisterScreen";
 
 type rootStackParamList = {
     Home: undefined;
+    Register: undefined;
 }
 
 const rootStack = createNativeStackNavigator<rootStackParamList>();
@@ -10,6 +12,7 @@ const rootStack = createNativeStackNavigator<rootStackParamList>();
 export default function RootStackNavigator() {
     return (
         < rootStack.Navigator >
+            <rootStack.Screen name="Register" component={RegisterScreen} options={{ title: "Registrera dig" }}></rootStack.Screen>
             <rootStack.Screen name="Home" component={HomeScreen}></rootStack.Screen>
         </rootStack.Navigator >
     );
