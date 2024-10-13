a665fab15d699f2bc4c15136ae4ca820c9138b35
Davod <davodnikoyii@gmail.com>
2024-10-13 14:27:40 +0200

    Created new reducer (householdReducer)

--- store/store.ts
+++ store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import householdReducer from "./householdReducer";
 import userReducer from "./userReducer";
 
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
+        household: householdReducer,
     },
 })
 
