a496d90a29878f2eb00d066bc898ec84a99e6e9e
Liam <04liagla@skola.boras.se>
2024-10-14 20:39:11 +0200

    Added Buttons and TextInputs

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,11 +1,56 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
 import { RootStackParamList } from '../navigators/RootStackNavigator';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
 
 export default function LoginScreen({ navigation }: Props) {
-  return <View></View>;
+  const [username, setUsername] = useState('');
+  const [pasword, setPassword] = useState('');
+
+  const hanldeLogin = null;
+  const navigateToRegister = null;
+
+  return (
+    <View>
+      <View style={{ padding: 14, gap: 14 }}>
+        <TextInput
+          style={s.textInput}
+          mode="outlined"
+          label={'Användarnamn'}
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+          theme={{ roundness: 10 }}
+        />
+        <TextInput
+          style={s.textInput}
+          mode="outlined"
+          label={'Lösenord'}
+          value={pasword}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry={true}
+          theme={{ roundness: 10 }}
+        />
+        <Button
+          title="Login"
+          mode="contained"
+          style={{ marginTop: 30 }}
+          onPress={hanldeLogin}
+        >
+          Logga in på konto
+        </Button>
+        <Button
+          title="Register"
+          mode="contained"
+          style={{ marginTop: 30 }}
+          onPress={navigateToRegister}
+        >
+          Registrera konto
+        </Button>
+      </View>
+    </View>
+  );
 }
 
 const s = StyleSheet.create({
