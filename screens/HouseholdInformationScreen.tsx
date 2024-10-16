038cffc544d40ae40711d8749ed0180bc1b018c7
Alireza177 <alirezamahdavi015@gmail.com>
2024-10-16 19:12:53 +0200

    add fotter buttons

--- screens/HouseholdInformationScreen.tsx
+++ screens/HouseholdInformationScreen.tsx
@@ -1,22 +1,62 @@
-import { ScrollView, StyleSheet } from 'react-native';
-import { Card, Text } from 'react-native-paper';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { Button, Card, Icon, Text } from 'react-native-paper';
 
 export default function HouseholdInformation() {
   return (
-    <ScrollView style={s.root}>
-      <Text style={s.text}>Din kod till hushållet</Text>
-      <Card style={s.box}>
-        <Card.Content style={{ height: 80 }}>
-          <Text>kod</Text>
-        </Card.Content>
-      </Card>
-      <Text style={s.text}>Ditt namn till hushållet</Text>
-      <Card style={s.box}>
-        <Card.Content style={{ height: 80 }}>
-          <Text>name</Text>
-        </Card.Content>
-      </Card>
-      <Text style={{ marginTop: 12, fontSize: 34 }}>Medlemmar:</Text>
+    <ScrollView contentContainerStyle={s.root}>
+      <View style={{ padding: 14 }}>
+        <Text style={s.text}>Din kod till hushållet</Text>
+        <Card style={s.box}>
+          <Card.Content style={{ height: 80 }}>
+            <Text style={s.text}>kod</Text>
+          </Card.Content>
+        </Card>
+        <Text style={s.text}>Ditt namn till hushållet</Text>
+        <Card style={s.box}>
+          <Card.Content style={{ height: 80 }}>
+            <Text style={s.text}>name</Text>
+          </Card.Content>
+        </Card>
+        <Text style={{ marginTop: 12, fontSize: 34 }}>Medlemmar:</Text>
+      </View>
+      <View style={s.footer}>
+        <Button
+          style={{ width: '50%' }}
+          mode="elevated"
+          textColor="black"
+          theme={{ roundness: 0 }}
+          icon={({ color }) => (
+            <Icon source="plus-circle-outline" size={27} color={color} />
+          )}
+          labelStyle={{
+            fontSize: 20,
+            lineHeight: 30,
+          }}
+          contentStyle={{ height: 65, gap: 10 }}
+          onPress={() => {}}
+        >
+          Spara
+        </Button>
+        <Button
+          style={{ width: '50%' }}
+          mode="elevated"
+          textColor="black"
+          theme={{ roundness: 0 }}
+          icon={({ color }) => (
+            <Icon source="close-circle-outline" size={27} color={color} />
+          )}
+          labelStyle={{
+            fontSize: 20,
+            lineHeight: 30,
+          }}
+          contentStyle={{ height: 65, gap: 10 }}
+          onPress={() => {
+            // navigation.goBack();
+          }}
+        >
+          Stäng
+        </Button>
+      </View>
     </ScrollView>
   );
 }
@@ -24,9 +64,7 @@ export default function HouseholdInformation() {
 const s = StyleSheet.create({
   root: {
     flex: 1,
-    // alignItems: 'flex-start',
-    // justifyContent: 'flex-start',
-    padding: 14,
+    justifyContent: 'space-between',
   },
   text: {
     marginTop: 12,
@@ -35,5 +73,9 @@ const s = StyleSheet.create({
   box: {
     width: '100%',
     marginTop: 12,
+  },
+  footer: {
+    flexDirection: 'row',
+    width: '100%',
   },
 });
