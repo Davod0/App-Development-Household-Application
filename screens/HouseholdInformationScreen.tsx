598ed868aab0f974d805f590ed12bf7c82028f61
Alireza177 <alirezamahdavi015@gmail.com>
2024-10-16 20:26:54 +0200

    Done with layout of household information (Without any logic)

--- screens/HouseholdInformationScreen.tsx
+++ screens/HouseholdInformationScreen.tsx
@@ -1,44 +1,55 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { ScrollView, StyleSheet, View } from 'react-native';
-import { Button, Card, Icon, Text } from 'react-native-paper';
+import { Button, Card, Icon, List, Text } from 'react-native-paper';
+import { RootStackParamList } from '../navigators/RootStackNavigator';
 
-export default function HouseholdInformation() {
+type Props = NativeStackScreenProps<RootStackParamList, 'HouseholdInformation'>;
+
+export default function HouseholdInformation({ navigation }: Props) {
   return (
     <ScrollView contentContainerStyle={s.root}>
-      <View style={{ padding: 14 }}>
-        <Text style={s.text}>Din kod till hushållet</Text>
-        <Card style={s.box}>
-          <Card.Content style={{ height: 80 }}>
-            <Text style={s.text}>kod</Text>
-          </Card.Content>
-        </Card>
-        <Text style={s.text}>Ditt namn till hushållet</Text>
-        <Card style={s.box}>
-          <Card.Content style={{ height: 80 }}>
-            <Text style={s.text}>name</Text>
-          </Card.Content>
-        </Card>
-        <Text style={{ marginTop: 12, fontSize: 34 }}>Medlemmar:</Text>
-      </View>
+      <ScrollView>
+        <View style={{ padding: 14 }}>
+          <Text style={s.text}>Din kod till hushållet</Text>
+          <Card style={s.box}>
+            <Card.Content style={{ height: 80 }}>
+              <Text style={s.text}>kod</Text>
+            </Card.Content>
+          </Card>
+          <Text style={s.text}>Ditt namn till hushållet</Text>
+          <Card style={s.box}>
+            <Card.Content style={{ height: 80 }}>
+              <Text style={s.text}>name</Text>
+            </Card.Content>
+          </Card>
+          <Text style={{ marginTop: 12, fontSize: 34 }}>Medlemmar:</Text>
+        </View>
+        <ScrollView>
+          <Card
+            style={{
+              marginTop: 12,
+              borderRadius: 0,
+            }}
+          >
+            <Card.Content
+              style={{
+                width: '100%',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+              }}
+            >
+              <List.Item
+                style={{ width: '50%' }}
+                title="Name"
+                left={(props) => <List.Icon {...props} icon="account-circle" />}
+              />
+            </Card.Content>
+          </Card>
+        </ScrollView>
+      </ScrollView>
       <View style={s.footer}>
         <Button
-          style={{ width: '50%' }}
-          mode="elevated"
-          textColor="black"
-          theme={{ roundness: 0 }}
-          icon={({ color }) => (
-            <Icon source="plus-circle-outline" size={27} color={color} />
-          )}
-          labelStyle={{
-            fontSize: 20,
-            lineHeight: 30,
-          }}
-          contentStyle={{ height: 65, gap: 10 }}
-          onPress={() => {}}
-        >
-          Spara
-        </Button>
-        <Button
-          style={{ width: '50%' }}
+          style={{ width: '100%' }}
           mode="elevated"
           textColor="black"
           theme={{ roundness: 0 }}
@@ -51,7 +62,7 @@ export default function HouseholdInformation() {
           }}
           contentStyle={{ height: 65, gap: 10 }}
           onPress={() => {
-            // navigation.goBack();
+            navigation.goBack();
           }}
         >
           Stäng
