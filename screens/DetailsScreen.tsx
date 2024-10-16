64c9fe8ce05747b31ac77c38cb2383a1a73a39de
Liam <04liagla@skola.boras.se>
2024-10-16 13:45:39 +0200

    add Surface and shadow

--- screens/DetailsScreen.tsx
+++ screens/DetailsScreen.tsx
@@ -1,7 +1,7 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { Surface, Text } from 'react-native-paper';
 import { RootStackParamList } from '../navigators/RootStackNavigator';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Details'>;
@@ -9,9 +9,45 @@ type Props = NativeStackScreenProps<RootStackParamList, 'Details'>;
 export default function DetailsScreen({ navigation }: Props) {
   return (
     <View>
-      <Text variant="displaySmall">Display Medium</Text>
-      <Text variant="displaySmall">Display Medium</Text>
-
+      <Surface
+        style={{
+          padding: 14,
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginBottom: 20,
+          margin: 20,
+        }}
+        elevation={1}
+      >
+        <Text variant="displaySmall">Display Medium</Text>
+      </Surface>
+      <Surface
+        style={{
+          padding: 140,
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginBottom: 20,
+          margin: 20,
+        }}
+        elevation={1}
+      >
+        <Text variant="displaySmall">Display Medium</Text>
+      </Surface>
     </View>
   );
 }
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    marginVertical: 1,
+  },
+  textInput: {
+    minHeight: 60,
+  },
+  footer: {
+    flexDirection: 'row',
+    width: '100%',
+  },
+});
