4d14ba19761a211bb59ec3f413fed6e110a407f4
Liam <04liagla@skola.boras.se>
2024-10-16 14:33:23 +0200

    add chip and meny

--- screens/DetailsScreen.tsx
+++ screens/DetailsScreen.tsx
@@ -1,7 +1,7 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 import { StyleSheet, View } from 'react-native';
-import { Surface, Text } from 'react-native-paper';
+import { Chip, Surface, Text } from 'react-native-paper';
 import { RootStackParamList } from '../navigators/RootStackNavigator';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Details'>;
@@ -17,6 +17,7 @@ export default function DetailsScreen({ navigation }: Props) {
           marginBottom: 20,
           margin: 20,
           borderRadius: 10,
+          backgroundColor: '#FFFFFF',
         }}
         elevation={1}
       >
@@ -30,10 +31,43 @@ export default function DetailsScreen({ navigation }: Props) {
           marginBottom: 20,
           margin: 20,
           borderRadius: 10,
+          backgroundColor: '#FFFFFF',
         }}
         elevation={1}
       >
         <Text variant="displaySmall">Display Medium</Text>
+      </Surface>
+      <Surface
+        style={{
+          padding: 14,
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginBottom: 20,
+          margin: 20,
+          borderRadius: 10,
+          backgroundColor: '#FFFFFF',
+        }}
+        elevation={1}
+      >
+        <Chip
+          onPress={() => console.log('Pressed')}
+          style={{ paddingVertical: 10, paddingHorizontal: 20 }}
+        >
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              width: '100%',
+            }}
+          >
+            <Text variant="displaySmall" style={{ textAlign: 'left' }}>
+              Återkommer
+            </Text>
+            <Text variant="displaySmall" style={{ textAlign: 'right' }}>
+              var dag
+            </Text>
+          </View>
+        </Chip>
       </Surface>
     </View>
   );
