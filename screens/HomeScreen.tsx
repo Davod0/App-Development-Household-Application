e607db43e0a3aa53445829e8c038b0dbee21fd41
mrKornelius <magnus.korneliusson@gmail.com>
2024-10-20 00:51:30 +0200

    styling...

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -50,7 +50,7 @@ export default function App({ navigation }: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
