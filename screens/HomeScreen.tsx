56d97ba7c2f1d7c63f307fe4a89ec76b81b2eb8a
mrKornelius <magnus.korneliusson@gmail.com>
2024-10-19 11:34:22 +0200

    added some temp links on home to other screens - for testing

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,8 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { RootStackParamList } from '../navigators/RootStackNavigator';
 // import { mockedHouseholds, mockedMembers } from '../data';
 // import { useAppSelector } from '../store/hooks';
 
-export default function App() {
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+export default function App({ navigation }: Props) {
   // const households = useAppSelector((state) => state.household.list);
   // const members = useAppSelector((state) => state.members.filter());
   // const householdMembers = useAppSelector(selectMembersByHousehold);
@@ -17,7 +20,29 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Pressable onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.text}>Login</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate('Profile')}>
+        <Text style={styles.text}>Profile</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate('Register')}>
+        <Text style={styles.text}>Register</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate('TestStore')}>
+        <Text style={styles.text}>TestStore</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate('CreateHouseHold')}>
+        <Text style={styles.text}>CreateHouseHold</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate('JoinHousehold')}>
+        <Text style={styles.text}>JoinHousehold</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate('SelectedHouseholdNav')}>
+        <Text style={styles.text}>SelectedHousehold</Text>
+      </Pressable>
+
+      {/* <Pressable onPress={() => navigation.navigate('Login')}><Text style={styles.text}>Login</Text></Pressable> */}
     </View>
   );
 }
@@ -28,5 +53,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 36,
   },
 });
