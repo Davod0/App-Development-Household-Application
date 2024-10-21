6f8791b4eca0464ea6448f686d0a86eac1c0bfe8
Alireza177 <alirezamahdavi015@gmail.com>
2024-10-22 01:27:41 +0200

    add switch cases foer error handling

--- store/user/userActions.ts
+++ store/user/userActions.ts
@@ -1,3 +1,4 @@
+import { FirebaseError } from 'firebase/app';
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -37,6 +38,27 @@ export const signInUser = createAppAsyncThunk<User, EmailPassword>(
       return result.user.toJSON() as User;
     } catch (error) {
       console.error(error);
+      if (error instanceof FirebaseError) {
+        let errorMessage = '';
+
+        switch (error.code) {
+          case 'auth/user-not-found':
+            errorMessage = 'Ingen användare hittades med denna e-postadress.';
+            break;
+          case 'auth/wrong-password':
+            errorMessage = 'Fel lösenord angivet. Försök igen.';
+            break;
+          case 'auth/invalid-email':
+            errorMessage = 'Ange en giltig e-postadress.';
+            break;
+          case 'auth/user-disabled':
+            errorMessage = 'Detta konto har inaktiverats. Kontakta support.';
+            break;
+          default:
+            errorMessage = 'Ett okänt fel uppstod. Försök igen senare.';
+        }
+        return thunkAPI.rejectWithValue(errorMessage);
+      }
       return thunkAPI.rejectWithValue(
         'Something went wrong, Could not register the user!:',
       );
