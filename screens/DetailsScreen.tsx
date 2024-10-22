ae7e571c0c2c9e38e59be8305c49801571dd7f8c
Liam <04liagla@skola.boras.se>
2024-10-22 08:54:32 +0200

    Flytta all styling till stylesheet för att göra koden tydligare

--- screens/DetailsScreen.tsx
+++ screens/DetailsScreen.tsx
@@ -1,5 +1,4 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Button, Icon, Surface, Text } from 'react-native-paper';
@@ -16,165 +15,50 @@ export default function DetailsScreen({ navigation }: Props) {
 
   return (
     <View style={s.container}>
-      <Surface
-        style={{
-          padding: 14,
-          alignItems: 'center',
-          justifyContent: 'center',
-          margin: 20,
-          borderRadius: 10,
-          backgroundColor: '#FFFFFF',
-        }}
-        elevation={1}
-      >
+      <Surface style={s.titleContainer} elevation={1}>
         <Text variant="displaySmall">{task?.name}</Text>
       </Surface>
-      <Surface
-        style={{
-          padding: 60,
-          alignItems: 'center',
-          justifyContent: 'center',
-          marginBottom: 10,
-          margin: 20,
-          borderRadius: 10,
-          backgroundColor: '#FFFFFF',
-        }}
-        elevation={1}
-      >
+      <Surface style={s.descriptionContainer} elevation={1}>
         <Text variant="headlineLarge">{task?.description}</Text>
       </Surface>
-      <Surface
-        style={{
-          padding: 14,
-          alignItems: 'center',
-          justifyContent: 'center',
-          marginBottom: 10,
-          margin: 20,
-          borderRadius: 10,
-          backgroundColor: '#FFFFFF',
-        }}
-        elevation={1}
-      >
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            width: '100%',
-          }}
-        >
+      <Surface style={s.frequencyContainer} elevation={1}>
+        <View style={s.frequencyContent}>
           <View>
-            <Text
-              variant="headlineSmall"
-              style={{ textAlign: 'left', fontWeight: 'bold' }}
-            >
+            <Text variant="headlineSmall" style={s.frequencyLabel}>
               Återkommer:
             </Text>
           </View>
-          <View
-            style={{
-              position: 'relative',
-              alignItems: 'center',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              width: '35%',
-            }}
-          >
-            <Text variant="headlineSmall" style={{ textAlign: 'right' }}>
+          <View style={s.frequencyValueContainer}>
+            <Text variant="headlineSmall" style={s.frequencyText}>
               var
             </Text>
-            <View
-              style={{
-                position: 'relative',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <View
-                style={{
-                  position: 'absolute',
-                  borderRadius: 15,
-                  borderColor: '#CD5D6F',
-                  borderWidth: 1,
-                  backgroundColor: '#CD5D6F',
-                  height: 30,
-                  width: 30,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-              />
-
+            <View style={s.frequencyCircle}>
               <Text variant="titleMedium" style={{ color: '#fff' }}>
                 {task?.frequency}
-                {''}
               </Text>
             </View>
-
-            <Text variant="headlineSmall" style={{ textAlign: 'right' }}>
+            <Text variant="headlineSmall" style={s.frequencyText}>
               dag
             </Text>
           </View>
         </View>
       </Surface>
-      <Surface
-        style={{
-          padding: 20,
-          alignItems: 'center',
-          justifyContent: 'center',
-
-          margin: 20,
-          borderRadius: 10,
-          backgroundColor: '#FFFFFF',
-        }}
-        elevation={1}
-      >
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            width: '100%',
-          }}
-        >
+      <Surface style={s.valueContainer} elevation={1}>
+        <View style={s.valueContent}>
           <View>
-            <Text
-              variant="headlineSmall"
-              style={{
-                textAlign: 'left',
-                textAlignVertical: 'top',
-                fontWeight: 'bold',
-              }}
-            >
+            <Text variant="headlineSmall" style={s.valueLabel}>
               Värde:
             </Text>
-            <Text
-              variant="titleMedium"
-              style={{ textAlign: 'left', textAlignVertical: 'bottom' }}
-            >
+            <Text variant="titleMedium" style={s.valueDescription}>
               Hur energikrävande är sysslan?
             </Text>
           </View>
-          <View
-            style={{
-              borderRadius: 30,
-              borderColor: '#F2F2F2',
-              borderWidth: 1,
-              backgroundColor: '#F2F2F2',
-              height: 30,
-              width: 30,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <Text
-              variant="headlineSmall"
-              style={{
-                textAlign: 'right',
-              }}
-            >
-              {task?.weight}
-            </Text>
+          <View style={s.valueCircle}>
+            <Text variant="headlineSmall">{task?.weight}</Text>
           </View>
         </View>
       </Surface>
+
       <View style={s.footer}>
         <Button
           style={{ width: '50%' }}
@@ -220,15 +104,101 @@ export default function DetailsScreen({ navigation }: Props) {
 const s = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
-    marginVertical: 1,
+    justifyContent: 'flex-start',
     backgroundColor: '#F2F2F2',
   },
-  textInput: {
-    minHeight: 60,
+  titleContainer: {
+    padding: 14,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    marginBottom: 5,
+    borderRadius: 10,
+    backgroundColor: '#FFFFFF',
+  },
+  descriptionContainer: {
+    padding: 14,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    marginBottom: 5,
+    borderRadius: 10,
+    backgroundColor: '#FFFFFF',
+  },
+  frequencyContainer: {
+    padding: 14,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    marginBottom: 5,
+    borderRadius: 10,
+    backgroundColor: '#FFFFFF',
+  },
+  frequencyContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  frequencyLabel: {
+    textAlign: 'left',
+    fontWeight: 'bold',
+  },
+  frequencyValueContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '35%',
+  },
+  frequencyText: {
+    textAlign: 'right',
+  },
+  frequencyCircle: {
+    borderRadius: 15,
+    borderColor: '#CD5D6F',
+    borderWidth: 1,
+    backgroundColor: '#CD5D6F',
+    height: 30,
+    width: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  valueContainer: {
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 20,
+    marginBottom: 5,
+    borderRadius: 10,
+    backgroundColor: '#FFFFFF',
+  },
+  valueContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  valueLabel: {
+    textAlign: 'left',
+    fontWeight: 'bold',
+  },
+  valueDescription: {
+    textAlign: 'left',
+    color: '#828282',
+  },
+  valueCircle: {
+    borderRadius: 30,
+    borderColor: '#F2F2F2',
+    borderWidth: 1,
+    backgroundColor: '#F2F2F2',
+    height: 30,
+    width: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   footer: {
     flexDirection: 'row',
     width: '100%',
+    paddingHorizontal: 10,
+    paddingBottom: 15,
+    justifyContent: 'space-between',
   },
 });
