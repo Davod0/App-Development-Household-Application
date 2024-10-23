ae2e4ccb3026df9518218ebb6ded1b735f3d0dd5
Alireza177 <alirezamahdavi015@gmail.com>
2024-10-23 15:20:48 +0200

    fix merge confilct on userReducer

--- store/user/usersReducer.ts
+++ store/user/usersReducer.ts
@@ -1,28 +1,62 @@
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
-import { mockedUsers, User } from '../../data';
-import { createUser } from './userActions';
+import { User } from 'firebase/auth';
+import { ColorMode } from '../../theme/ThemeProvider';
+import { signInUser, signUpUser } from './userActions';
 
-const initialState: User[] = mockedUsers;
+type userState = {
+  currentUser?: User;
+  isLoading: boolean;
+  registerErrorMessage?: string;
+  loginErrorMessage?: string;
+  theme: ColorMode;
+};
+const initialState: userState = {
+  currentUser: undefined,
+  isLoading: true,
+  theme: 'auto',
+};
 
-export const usersSlice = createSlice({
+export const userSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {
-    createUserOptimistically: (state, action: PayloadAction<User>) => {
-      state.push(action.payload);
+    setUserOptimistically: (state, action: PayloadAction<User>) => {
+      state.currentUser = action.payload;
+      state.isLoading = false;
     },
-    clearUserOptimistically: (state, action: PayloadAction<User>) => {
-      state.filter((user) => user.id !== action.payload.id);
+    setColorMode: (state, action: PayloadAction<ColorMode>) => {
+      state.theme = action.payload;
     },
   },
-  extraReducers: (builder) => {
-    builder.addCase(createUser.fulfilled, (state, action) => {
-      state.push(action.payload);
+
+  extraReducers: async (builder) => {
+    builder.addCase(signUpUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(signUpUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+    });
+    builder.addCase(signUpUser.rejected, (state, action) => {
+      // TODO: fix as string?!
+      state.registerErrorMessage = action.payload as string;
+      state.isLoading = false;
+      console.log(`Error message from user reducer:1 ${action.payload}`);
+    });
+    builder.addCase(signInUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(signInUser.fulfilled, (state, action) => {
+      state.isLoading = false;
+    });
+    builder.addCase(signInUser.rejected, (state, action) => {
+      // TODO: fix as string?!
+      state.loginErrorMessage = action.payload as string;
+      state.isLoading = false;
+      console.log(`Error message from user reducer:2 ${action.payload}`);
     });
   },
 });
 
-export const { createUserOptimistically, clearUserOptimistically } =
-  usersSlice.actions;
-export default usersSlice.reducer;
+export const { setUserOptimistically, setColorMode } = userSlice.actions;
+export default userSlice.reducer;
