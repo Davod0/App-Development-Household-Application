3c92a2066f76ea399f8044d5b107472807f303dc
Turtle <gustavsson.sofia@hotmail.com>
2024-10-24 20:58:27 +0200

    add button style

--- screens/EditTaskScreen.tsx
+++ screens/EditTaskScreen.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Surface, TextInput } from 'react-native-paper';
+import { Button, Icon, Surface, TextInput } from 'react-native-paper';
 import DatePicker from '../components/DatePicker';
 import EffortPicker from '../components/EffortPicker';
 import { Task } from '../types';
@@ -28,30 +28,76 @@ export default function EditTaskScreen() {
   const [weight, setWeight] = useState(testTask.weight);
   return (
     <View style={s.container}>
-      <Surface>
-        <TextInput label="Namn" value={name} onChangeText={setName} />
-      </Surface>
+      <View style={s.inputContainer}>
+        <Surface>
+          <TextInput label="Namn" value={name} onChangeText={setName} />
+        </Surface>
 
-      <Surface>
-        <TextInput
-          label="Beskrivning"
-          multiline={true}
-          numberOfLines={5}
-          value={description}
-          onChangeText={setDescription}
-        />
-      </Surface>
+        <Surface>
+          <TextInput
+            label="Beskrivning"
+            multiline={true}
+            numberOfLines={5}
+            value={description}
+            onChangeText={setDescription}
+          />
+        </Surface>
 
-      <DatePicker frequency={frequency} setFrequency={setFrequency} />
+        <DatePicker frequency={frequency} setFrequency={setFrequency} />
 
-      <EffortPicker weight={weight} setWeight={setWeight} />
+        <EffortPicker weight={weight} setWeight={setWeight} />
+      </View>
+      <View style={s.buttonContainer}>
+        <Button
+          mode="elevated"
+          theme={{ roundness: 0 }}
+          style={s.button}
+          labelStyle={s.buttonText}
+          icon={({ color }) => (
+            <Icon source="check-circle-outline" size={27} color={color} />
+          )}
+        >
+          Spara
+        </Button>
+        <Button
+          mode="elevated"
+          theme={{ roundness: 0 }}
+          style={s.button}
+          labelStyle={s.buttonText}
+          icon={({ color }) => (
+            <Icon source="close-circle-outline" size={27} color={color} />
+          )}
+        >
+          Stäng
+        </Button>
+      </View>
     </View>
   );
 }
 
 const s = StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+
+  inputContainer: {
     margin: 15,
     gap: 15,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '50%',
+    fontSize: 20,
+  },
+  buttonText: {
+    fontSize: 20,
+    lineHeight: 30,
   },
 });
