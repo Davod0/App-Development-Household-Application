acde419c67d02069cb341b9573038d708d371a99
Alireza177 <alirezamahdavi015@gmail.com>
2024-10-25 09:24:56 +0200

    make code more understandable

--- store/Members/memberSlice.ts
+++ store/Members/memberSlice.ts
@@ -19,39 +19,41 @@ const membersSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder
-      .addCase(addMember.fulfilled, (state, action: PayloadAction<Member>) => {
+    builder.addCase(
+      addMember.fulfilled,
+      (state, action: PayloadAction<Member>) => {
         state.push(action.payload);
-      })
-      .addCase(addMember.rejected, (state, action) => {
-        console.error('Failed to add member: ', action.payload);
-      })
-      .addCase(
-        deleteMemberById.fulfilled,
-        (state, action: PayloadAction<string>) => {
-          return state.filter((member) => member.id !== action.payload);
-        },
-      )
-      .addCase(deleteMemberById.rejected, (state, action) => {
-        console.error('Failed to delete member: ', action.payload);
-      })
-      .addCase(
-        updateMember.fulfilled,
-        (state, action: PayloadAction<Member>) => {
-          const index = state.findIndex(
-            (member) => member.id === action.payload.id,
-          );
-          if (index !== -1) {
-            state[index] = action.payload;
-          }
-        },
-      )
-      .addCase(updateMember.rejected, (state, action) => {
-        console.error('Failed to update member: ', action.payload);
-      })
-      .addCase(getAllMembers.fulfilled, (state, action) => {
-        state = action.payload;
-      });
+      },
+    );
+    builder.addCase(addMember.rejected, (state, action) => {
+      console.error('Failed to add member: ', action.payload);
+    });
+    builder.addCase(
+      deleteMemberById.fulfilled,
+      (state, action: PayloadAction<string>) => {
+        return state.filter((member) => member.id !== action.payload);
+      },
+    );
+    builder.addCase(deleteMemberById.rejected, (state, action) => {
+      console.error('Failed to delete member: ', action.payload);
+    });
+    builder.addCase(
+      updateMember.fulfilled,
+      (state, action: PayloadAction<Member>) => {
+        const index = state.findIndex(
+          (member) => member.id === action.payload.id,
+        );
+        if (index !== -1) {
+          state[index] = action.payload;
+        }
+      },
+    );
+    builder.addCase(updateMember.rejected, (state, action) => {
+      console.error('Failed to update member: ', action.payload);
+    });
+    builder.addCase(getAllMembers.fulfilled, (state, action) => {
+      state = action.payload;
+    });
   },
 });
 
