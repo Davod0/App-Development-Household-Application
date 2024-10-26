e36a7c637374dcd56f96957639861f8d5ec4092e
mrKornelius <magnus.korneliusson@gmail.com>
2024-10-27 00:07:57 +0200

    added request actions

--- store/requests/actions.ts
+++ store/requests/actions.ts
@@ -1,4 +1,12 @@
-import { collection, doc, setDoc } from 'firebase/firestore';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  where,
+} from 'firebase/firestore';
 import { db } from '../../firebase';
 import { CreateRequest, Request } from '../../types';
 import { createAppAsyncThunk } from '../hooks';
@@ -21,5 +29,39 @@ export const addRequest = createAppAsyncThunk<Request, CreateRequest>(
   },
 );
 
-//deleteRequest
-//getRequestsByHouseholdId
+export const getRequestsBySelectedHouseholdId = createAppAsyncThunk<Request[]>(
+  'requests/getBySelectedHouseholdId',
+  async (_, thunkApi) => {
+    const state = thunkApi.getState();
+    try {
+      const snapshot = await getDocs(
+        query(
+          collection(db, 'requests'),
+          where('householdId', '==', state.user.selectedHousehold?.id!),
+        ),
+      );
+      const data: Request[] = [];
+      snapshot.forEach((doc) => {
+        data.push(doc.data() as Request);
+      });
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(`Error retrieving requests: ${error}`);
+    }
+  },
+);
+
+export const deleteRequest = createAppAsyncThunk<Request, Request>(
+  'requests/delete',
+  async (request, thunkApi) => {
+    console.log(request);
+
+    try {
+      const docRef = doc(db, 'requests', request.id);
+      await deleteDoc(docRef);
+      return request;
+    } catch (error) {
+      return thunkApi.rejectWithValue(`Error deleting requests: ${error}`);
+    }
+  },
+);
