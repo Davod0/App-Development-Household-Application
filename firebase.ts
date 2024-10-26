20c007989e3dd168d9e6dadb575936594217265a
Alireza177 <alirezamahdavi015@gmail.com>
2024-10-27 00:56:28 +0200

    show members in household info screen

--- firebase.ts
+++ firebase.ts
@@ -4,6 +4,15 @@ import { initializeApp } from 'firebase/app';
 import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+// const firebaseConfig = {
+//   apiKey: process.env.EXPO_PUBLIC_FIREBAS_API,
+//   authDomain: 'fire-base-db-90846.firebaseapp.com',
+//   projectId: 'fire-base-db-90846',
+//   storageBucket: 'fire-base-db-90846.appspot.com',
+//   messagingSenderId: '1084251965186',
+//   appId: '1:1084251965186:web:aad9371bc24596042a641b',
+// };
+
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBAS_API,
   authDomain: 'fire-base-db-90846.firebaseapp.com',
