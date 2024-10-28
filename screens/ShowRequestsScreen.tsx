a5e2eb545af69aa61e755a26d0f299595a6b5f29
Toxex <tonyhallentorp@gmail.com>
2024-10-28 11:31:13 +0100

    think uses correct selector

--- screens/ShowRequestsScreen.tsx
+++ screens/ShowRequestsScreen.tsx
@@ -1,8 +1,13 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigators/RootStackNavigator';
 import { View } from 'react-native';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { selectAllRequests } from '../store/requests/requestsSelectors';
 
 export default function ShowRequestsScreen() {
   type Props = NativeStackScreenProps<RootStackParamList, 'ShowRequests'>;
+  const dispatch = useAppDispatch();
+  const requests = useAppSelector(selectAllRequests);
+
   return <View></View>;
 }
