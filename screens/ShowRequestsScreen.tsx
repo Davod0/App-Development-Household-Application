66da3d6049886bee6e346f0cda78ce32c570b326
Toxex <tonyhallentorp@gmail.com>
2024-10-28 11:48:39 +0100

    added logic for no request loaded (i think) and also a list of requests with req id and member id

--- screens/ShowRequestsScreen.tsx
+++ screens/ShowRequestsScreen.tsx
@@ -1,13 +1,35 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigators/RootStackNavigator';
-import { View } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
-import { selectAllRequests } from '../store/requests/requestsSelectors';
+import {
+  selectAllRequests,
+  selectRequestError,
+} from '../store/requests/requestsSelectors';
+import { Text } from 'react-native-paper';
 
 export default function ShowRequestsScreen() {
   type Props = NativeStackScreenProps<RootStackParamList, 'ShowRequests'>;
-  const dispatch = useAppDispatch();
+  // const dispatch = useAppDispatch();
   const requests = useAppSelector(selectAllRequests);
+  const noRequest = useAppSelector(selectRequestError);
 
-  return <View></View>;
+  return !requests ? (
+    <View>{noRequest}</View>
+  ) : (
+    <View style={{ flex: 1, padding: 16 }}>
+      <FlatList
+        data={requests}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{ padding: 10, borderBottomWidth: 1, borderColor: '#ccc' }}
+          >
+            <Text style={{ fontSize: 18 }}>{item.id}</Text>
+            <Text style={{ color: '#666' }}>{item.memberId}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
 }
