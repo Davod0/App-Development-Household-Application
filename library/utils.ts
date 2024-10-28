c941fd4463870261b747546c59c9d94064647b12
mrKornelius <magnus.korneliusson@gmail.com>
2024-10-28 18:30:45 +0100

    created a name shortener function

--- library/utils.ts
+++ library/utils.ts
@@ -17,3 +17,19 @@ export const generateRandomCode = () => {
   }
   return result;
 };
+
+/**
+ * A function that will slice off the end of a long string, and add ellipsis (...) to the
+ * end if the string is sliced.
+ * @param str a text string that can be undefined
+ * @param len the max length you want the text to be
+ * @returns a string that is max len characters long or an empty string if str is undefined.
+ */
+export function sliceStringToLengthAddEllipsis(
+  str: string | undefined,
+  len: number,
+) {
+  if (str && str.length > len)
+    return str.slice(0, len - 3).trim() + (str.length > len - 3 ? '...' : '');
+  return '';
+}
